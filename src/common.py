a953af0b1d9d96e7cda0ec52e946b6a06a52043f
Zkh-dot <kernzahar@gmail.com>
2025-10-14 13:54:28 +0200

    propper end of month dates

--- src/common.py
+++ src/common.py
@@ -2,6 +2,7 @@ import discord
 import CONSTANTS
 import datatypes
 import re
+import datetime
 import db_worker as dbw
 
 async def get_user_by_id(client: discord.Client, guild_id: int, user_id: int, db_worker: dbw.DBWorker = None) -> datatypes.User:
@@ -25,13 +26,15 @@ async def get_user_by_id(client: discord.Client, guild_id: int, user_id: int, db
                 member = None
             except discord.Forbidden:
                 member = None
-        else:
-            if member.joined_at:
-                delta = discord.utils.utcnow() - member.joined_at
-                need_to_get = min(45, int(delta.days * 1.5))
     liable = 1
     is_member = 0
     if member:
+        next_month = (discord.utils.utcnow().replace(day=28) + datetime.timedelta(days=4)).replace(hour=0, minute=0, second=0, microsecond=0)
+        last_day = next_month - datetime.timedelta(days=next_month.day)
+
+        delta = last_day - member.joined_at
+        print(f"member {member.display_name} joined_at: {member.joined_at.isoformat()}, delta: {delta}")
+        need_to_get = min(45, int(delta.days * 1.5))
         is_member = 1
         liable = 0 if (CONSTANTS.RENTOR_NAME in [r.name for r in member.roles if r.name != "@everyone"]) else 1
     try:
